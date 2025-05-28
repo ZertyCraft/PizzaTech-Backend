d5d215bd36bbc41464e4ae2ed5b59d841c49b02c
Gaëtan <gaetan.didier@outlook.com>
2025-05-28 14:45:29 +0200

    feat: add user and pizza repository interfaces

---
+++ internal/domain/repositories/user_repository.go
@@ -0,0 +1,9 @@
+package repositories
+
+import "pizzatech/internal/domain/models"
+
+type UserRepository interface {
+	Create(user *models.User) error
+	FindByEmail(email string) (*models.User, error)
+	FindByID(id uint) (*models.User, error)
+}
